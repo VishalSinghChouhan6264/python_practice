0e062d42c47c2588352fa6e26f01e746014acf2e
VishalSinghChouhan6264 <vrajput5894@gmail.com>
2022-11-28 16:47:33 +0530

    first

---
+++ vishal.py
@@ -0,0 +1,17 @@
+def prime(n):
+     d=0
+     for i in range(2,n):
+          if n%i==0:
+               d=1
+               break
+     if d==1:
+         return "not a prime"
+     else:
+         return "a prime"
+def factorial(n):
+    d=1
+    for i in range(1,n+1):
+        d=d*i
+    return d
+     
+     
